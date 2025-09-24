74221085334f85f2deb0984e61e4ed33e9b44932
Ant <anthonyreyes12.06.25.dignitatec@gmail.com>
2025-09-24 12:24:25 -0500

    Fix: Corregir rutas para GitHub Pages y reemplazar video con fondo animado

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://estremix-01100001.github.io',
-  base: '/servicios-generales-nutech',
+  site: 'https://cristianalejandro12.github.io',
+  base: '/website',
   output: 'static',
-  build: {
-    assets: 'assets'
-  },
-  integrations: [tailwind()],
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          assetFileNames: 'assets/[name].[hash][extname]',
-          chunkFileNames: 'assets/[name].[hash].js',
-          entryFileNames: 'assets/[name].[hash].js'
-        }
-      }
-    }
-  }
+  integrations: [tailwind()]
 });
